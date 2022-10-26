a194fdfd7aa7a244cc0f1123a91e101362295536
Cheung <cheung@tdfcu.com>
2022-10-26 11:23:49 -0400

    changed window size and added button to convert miles to km

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import tkinter
 window = tkinter.Tk()
 window.title("Mile to Km Converter")
-window.minsize(width=400, height=300)
+window.minsize(width=300, height=200)
+window.config(padx=50, pady=50)
 
 input = tkinter.Entry(width=10)
 input.insert(tkinter.END, '0')
@@ -19,5 +20,14 @@ converted_metric_label.grid(row=1, column=1)
 km_label = tkinter.Label(text="Km", font=('Arial', 12))
 km_label.grid(row=1, column=2)
 
+
+def calculate():
+    miles = float(input.get())
+    km = miles * 1.609
+    return converted_metric_label.config(text=km)
+
+
+button = tkinter.Button(text='Calculate', command=calculate)
+button.grid(row=2, column=1)
 
 window.mainloop()
